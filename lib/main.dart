8062a123cfb5da46e06b5c615985ce3a12576f7e
SabianF <58588133+SabianF@users.noreply.github.com>
2021-10-16 20:42:53 -0700

    implemented wordPair generator package
    - based on 30:55 from https://youtu.be/1gDhl4leEzA?t=1855

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.green,
@@ -12,6 +16,9 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Wordpair Generator"),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase, style: TextStyle(fontSize: 32)),
         ),
       ),
     );
